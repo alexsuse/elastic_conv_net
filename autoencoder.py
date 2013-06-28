7c03241b261b385da9cd2d70925c3c67eb70e4a4
chausler <chausler@gmail.com>
2013-06-28 11:52:28 +0200

    style updates and project added

--- autoencoder.py
+++ autoencoder.py
@@ -3,177 +3,195 @@
 Implements a simple autoencoder class, with a simple training method.
 """
 from theano import tensor as T
-from theano import function, shared
-from numpy import exp, dot, sum, log
+from theano import shared
 import numpy as np
 import theano
 import load_data as ld
-import matplotlib.pyplot as plt
 import cPickle as pic
 
 
 class dA:
-	"""
-	The classic autoencoder of yore.
-	hidden activation is given by h = logistic(W_vtoh*visible+b_h)
-	visible activation is givens by z = logistic(W_htov*hidden+b_v)
-	"""
-	def __init__(self,nvisible,nhidden,data = None, W1 = None, bv= None, bh = None, rng_seed = None, regL = None):
-		self.nhidden = nhidden
-		self.nvisible = nvisible
+    """
+    The classic autoencoder of yore.
+    hidden activation is given by h = logistic(W_vtoh*visible+b_h)
+    visible activation is givens by z = logistic(W_htov*hidden+b_v)
+    """
+    def __init__(self, nvisible, nhidden, data=None, W1=None, bv=None,
+                 bh=None, rng_seed=None, regL=None):
+        self.nhidden = nhidden
+        self.nvisible = nvisible
 
-		#hidden to visible matrix
-		if W1 == None:
-			Wi = np.asarray(np.random.uniform(
-				low = -4*np.sqrt(6./(nhidden+nvisible) ),
-				high = 4*np.sqrt(6./(nhidden+nvisible) ),
-				size = (nvisible,nhidden)),dtype = theano.config.floatX)
-			W = shared(value = Wi, name = 'W')
-		else:
-			W = shared(value = W1, name = 'W')
-		self.W = W
-		self.Wprime = W.T
+        #hidden to visible matrix
+        if W1 == None:
+            Wi = np.asarray(np.random.uniform(
+                low=-4 * np.sqrt(6. / (nhidden + nvisible)),
+                high=4 * np.sqrt(6. / (nhidden + nvisible)),
+                size=(nvisible, nhidden)), dtype=theano.config.floatX)
+            W = shared(value=Wi, name='W')
+        else:
+            W = shared(value=W1, name='W')
+        self.W = W
+        self.Wprime = W.T
 
-		#biases
-		if bh==None:
-			bi_h = np.asarray(np.zeros(nhidden),dtype = theano.config.floatX)
-			b_h = shared(value = bi_h , name = 'b_h')
-		else:
-			b_h = shared(value = bh , name = 'b_h')
-		if bv==None:
-			bi_v = np.asarray(np.zeros(nvisible),dtype = theano.config.floatX)
-			b_v = shared(value = bi_v , name= 'b_v')
-		else:
-			b_v = shared(value = bv , name = 'b_v')
-		self.b_v = b_v
-		self.b_h = b_h
-		
-		#regularization parameter lambda
-		
-		if regL==None:
-			self.lamb = None
-		else:
-			self.lamb = shared(value = regL, name = 'lamb')
-		if data:
-			self.data = data
-		else:
-			self.data = T.dmatrix('data')
-		if rng_seed:
-			self.rng = np.random.RandomState(rng_seed)
-			self.theano_rng = T.shared_randomstreams.RandomStreams(self.rng.randint(2**30))
-		else:
-			self.rng = np.random.RandomState(1234)
-			self.theano_rng = T.shared_randomstreams.RandomStreams(self.rng.randint(2**30))
+        #biases
+        if bh == None:
+            bi_h = np.asarray(np.zeros(nhidden), dtype=theano.config.floatX)
+            b_h = shared(value=bi_h, name='b_h')
+        else:
+            b_h = shared(value=bh, name='b_h')
+        if bv == None:
+            bi_v = np.asarray(np.zeros(nvisible), dtype=theano.config.floatX)
+            b_v = shared(value=bi_v, name='b_v')
+        else:
+            b_v = shared(value=bv, name='b_v')
+        self.b_v = b_v
+        self.b_h = b_h
 
-		self.params=[self.W,self.b_h,self.b_v]
+        #regularization parameter lambda
+        if regL == None:
+            self.lamb = None
+        else:
+            self.lamb = shared(value=regL, name='lamb')
+        if data:
+            self.data = data
+        else:
+            self.data = T.dmatrix('data')
+        if rng_seed:
+            self.rng = np.random.RandomState(rng_seed)
+            self.theano_rng = T.shared_randomstreams.RandomStreams(
+                                                self.rng.randint(2 ** 30))
+        else:
+            self.rng = np.random.RandomState(1234)
+            self.theano_rng = T.shared_randomstreams.RandomStreams(
+                                                self.rng.randint(2 ** 30))
 
-	def get_reconstruction_function(self,input):
-		return T.nnet.sigmoid(T.dot(T.nnet.sigmoid(T.dot(input,self.W)+self.b_h),self.Wprime)+self.b_v)
+        self.params = [self.W, self.b_h, self.b_v]
 
-	def corrupt_input(self,input,corruption_level):
-		return self.theano_rng.binomial(size=input.shape, n=1, p=1 - corruption_level) * input
+    def get_reconstruction_function(self, input_val):
+        return T.nnet.sigmoid(T.dot(T.nnet.sigmoid(T.dot(input_val, self.W)
+                                        + self.b_h), self.Wprime) + self.b_v)
 
-	def get_cost_and_updates(self,corruptionlevel,learning_rate):
+    def corrupt_input(self, input_val, corruption_level):
+        return self.theano_rng.binomial(size=input_val.shape, n=1,
+                                    p=(1 - corruption_level)) * input_val
 
-		reconst_x = self.get_reconstruction_function(self.corrupt_input(self.data,corruptionlevel))
-		L = -T.sum(self.data*T.log(reconst_x)+(1-self.data)*T.log(1-reconst_x),axis=1)
-		cost = T.mean(L)
-		if self.lamb!=None:
-			L += self.lamb*(T.mean(T.dot(self.W,self.W)))
-		
-		gparams = T.grad(cost,self.params)
+    def get_cost_and_updates(self, corruptionlevel, learning_rate):
 
-		updates=[]
-		for param,gparams in zip(self.params,gparams):
-			updates.append((param,param-learning_rate*gparams))
-	
-		return (cost,updates)
+        reconst_x = self.get_reconstruction_function(
+                            self.corrupt_input(self.data, corruptionlevel))
+        L = -T.sum(self.data * T.log(reconst_x)
+                   + (1 - self.data) * T.log(1 - reconst_x), axis=1)
+        cost = T.mean(L)
+        if self.lamb != None:
+            L += self.lamb * (T.mean(T.dot(self.W, self.W)))
+
+        gparams = T.grad(cost, self.params)
+
+        updates = []
+        for param, gparams in zip(self.params, gparams):
+            updates.append((param, param - learning_rate * gparams))
+
+        return (cost, updates)
+
 
 class assymetric_dA(dA):
-	"""
-	Assymetric AE, extend from dA
-	hidden activation is given by h = logistic(W_vtoh*visible+b_h)
-	visible activation is givens by z = logistic(W_htov*hidden+b_v)
-	"""
-	def __init__(self,nvisible,nhidden,data = None, W1 = None, W2 = None, b_v = None, b_h = None, rng_seed = None, regL = None):
-		dA.__init__(self,nvisible,nhidden,data,W1,b_v,b_h,rng_seed,regL)
+    """
+    Assymetric AE, extend from dA
+    hidden activation is given by h = logistic(W_vtoh*visible+b_h)
+    visible activation is givens by z = logistic(W_htov*hidden+b_v)
+    """
+    def __init__(self, nvisible, nhidden, data=None, W1=None, W2=None,
+                 b_v=None, b_h=None, rng_seed=None, regL=None):
+        dA.__init__(self, nvisible, nhidden, data, W1, b_v, b_h,
+                    rng_seed, regL)
 
-		#visible to hidden matrix
-		Wi_htov = np.asarray(np.random.uniform(
-			low = -4*np.sqrt(6./(nhidden+nvisible) ),
-			high = 4*np.sqrt(6./(nhidden+nvisible) ),
-			size = (nhidden,nvisible)),dtype = theano.config.floatX)
-		Wprime = shared(value = Wi_htov, name = 'Wprime')
-		self.Wprime = Wprime
-		
-		self.params.append(self.Wprime)
-	
-	def get_cost_and_updates(self,corruptionlevel,learning_rate):
+        #visible to hidden matrix
+        Wi_htov = np.asarray(np.random.uniform(
+            low=-4 * np.sqrt(6. / (nhidden + nvisible)),
+            high=4 * np.sqrt(6. / (nhidden + nvisible)),
+            size=(nhidden, nvisible)), dtype=theano.config.floatX)
+        Wprime = shared(value=Wi_htov, name='Wprime')
+        self.Wprime = Wprime
+        self.params.append(self.Wprime)
 
-		reconst_x = self.get_reconstruction_function(self.corrupt_input(self.data,corruptionlevel))
-		L = -T.sum(self.data*T.log(reconst_x)+(1-self.data)*T.log(1-reconst_x),axis=1)
-		cost = T.mean(L)
-		if self.lamb!=None:
-			L += self.lamb*(T.mean(T.dot(self.W,self.W))+T.mean(T.dot(self.Wprime,self.Wprime)))
-		
-		gparams = T.grad(cost,self.params)
+    def get_cost_and_updates(self, corruptionlevel, learning_rate):
 
-		updates=[]
-		for param,gparams in zip(self.params,gparams):
-			updates.append((param,param-learning_rate*gparams))
-	
-		return (cost,updates)
+        reconst_x = self.get_reconstruction_function(
+                                self.corrupt_input(self.data, corruptionlevel))
+        L = -T.sum(self.data * T.log(reconst_x)
+                   + (1 - self.data) * T.log(1 - reconst_x), axis=1)
+        cost = T.mean(L)
+        if self.lamb != None:
+            L += self.lamb * (T.mean(T.dot(self.W, self.W))
+                              + T.mean(T.dot(self.Wprime, self.Wprime)))
 
-if __name__=='__main__':
-	#Mnist has 70000 examples, we use 50000 for training, set 20000 aside for validation
-	train_size = 50000
-	train_data,validation_data = ld.load_data_mnist(train_size = train_size)
+        gparams = T.grad(cost, self.params)
 
-	#fiddle around, not sure which values to use
-	training_epochs = 100
-	training_batches=100
-	patch_size = 10
-	batch_size = int(train_data['images'].shape[0]/training_batches)
-	batches = ld.make_vector_patches(train_data,training_batches,batch_size,patch_size)
-	validation_images = ld.make_vector_patches(validation_data,1,validation_data['images'].shape[0],patch_size)
-	#batches,ys = ld.make_vector_patches(train_data,training_batches,batch_size,patch_size)
-	#validation_images,validation_ys = ld.make_vector_batches(validation_data,1,validation_data['images'].shape[0])
+        updates = []
+        for param, gparams in zip(self.params, gparams):
+            updates.append((param, param - learning_rate * gparams))
 
-	index = T.lscalar()
-	x = T.dmatrix('x')
-	
-	#Creates a denoising autoencoder with 500 hidden nodes, could be changed as well
-	a = dA(100,500,data=x,regL=0.05)
-	
-	#sEt theano shared variables for the train and validation data
-	data_x = theano.shared(value = np.asarray(batches,dtype=theano.config.floatX),name='data_x')
-	validation_x = theano.shared(value = np.asarray(validation_images[0,:,:],dtype=theano.config.floatX),name='validation_x')
+        return (cost, updates)
 
-	#get cost and update functions for the autoencoder
-	cost,updates = a.get_cost_and_updates(0.4,0.05)
+if __name__ == '__main__':
+    #Mnist has 70000 examples, we use 50000 for training
+    # set 20000 aside for validation
+    train_size = 50000
+    train_data, validation_data = ld.load_data_mnist(train_size=train_size)
 
-	#train_da returns the current cost and updates the dA parameters, index gives the batch index.
-	train_da = theano.function([index],cost,updates=updates,givens=[(x , data_x[index])],on_unused_input='ignore')
+    #fiddle around, not sure which values to use
+    training_epochs = 100
+    training_batches = 100
+    patch_size = 10
+    batch_size = int(train_data['images'].shape[0] / training_batches)
+    batches = ld.make_vector_patches(train_data, training_batches,
+                                     batch_size, patch_size)
+    validation_images = ld.make_vector_patches(validation_data, 1,
+                                validation_data['images'].shape[0], patch_size)
+    #batches,ys = ld.make_vector_patches(train_data,training_batches,batch_size,patch_size)
+    #validation_images,validation_ys = ld.make_vector_batches(validation_data,1,validation_data['images'].shape[0])
 
-	#validation_error just returns the cost on the validation set
-	validation_error = theano.function([],cost,givens=[(x,validation_x)],on_unused_input='ignore')
+    index = T.lscalar()
+    x = T.dmatrix('x')
 
-	#loop over training epochs
-	for epoch in xrange(training_epochs):
-		c = []
-		ve = validation_error()	
-		#loop over batches
-		for batch in xrange(training_batches):
-			
-			#collect costs for this batch
-			c.append(train_da(batch))
+    #Creates a denoising autoencoder with 500 hidden nodes
+    a = dA(100, 500, data=x, regL=0.05)
 
-		#pritn mean training cost in this epoch and final validation cost for checking
-		print 'Training epoch %d, cost %lf, validation cost %lf' % (epoch, np.mean(c), ve)
-	#try:
-	#	finame = raw_input('Output to pickle?')
-	#except SyntaxError, NameError:
-	finame = 'output_pickle_2'
-	fi = open(finame,'w')
-	b = [a.W.get_value(),a.b_h.get_value(),a.b_v.get_value()]
-	pic.dump(b,fi)
+    #sEt theano shared variables for the train and validation data
+    data_x = theano.shared(value=np.asarray(batches,
+                                    dtype=theano.config.floatX), name='data_x')
+    validation_x = theano.shared(value=np.asarray(validation_images[0, :, :],
+                            dtype=theano.config.floatX), name='validation_x')
+
+    #get cost and update functions for the autoencoder
+    cost, updates = a.get_cost_and_updates(0.4, 0.05)
+
+    #train_da returns the current cost and updates the dA parameters,
+    #index gives the batch index.
+    train_da = theano.function([index], cost, updates=updates,
+                        givens=[(x, data_x[index])], on_unused_input='ignore')
+
+    #validation_error just returns the cost on the validation set
+    validation_error = theano.function([], cost,
+                        givens=[(x, validation_x)], on_unused_input='ignore')
+
+    #loop over training epochs
+    for epoch in xrange(training_epochs):
+        c = []
+        ve = validation_error()
+        #loop over batches
+        for batch in xrange(training_batches):
+            #collect costs for this batch
+            c.append(train_da(batch))
+
+        #print mean training cost in this epoch
+        #and final validation cost for checking
+        print 'Training epoch %d, cost %lf, validation cost %lf' % (epoch,
+                                                            np.mean(c), ve)
+    #try:
+    #    finame = raw_input('Output to pickle?')
+    #except SyntaxError, NameError:
+    finame = 'output_pickle_2'
+    fi = open(finame, 'w')
+    b = [a.W.get_value(), a.b_h.get_value(), a.b_v.get_value()]
+    pic.dump(b, fi)
--- load_data.py
+++ load_data.py
@@ -2,105 +2,118 @@
 """
 load mnist data from skdata library
 """
-
 from skdata.mnist import view
 from numpy.random import permutation
 from numpy import reshape, prod
 import numpy as np
 
+
 def load_data_mnist(train_size=None):
-	"""
-	load the mnist dataset from the skdata package.
-	"""
-	print "---->\n.....Loading MNIST data from the skdata package"
-	dataset = view.OfficialImageClassification()
-	if train_size:
-		train_data = {}
-		validation_data = {}
-		order = permutation(dataset.all_images.shape[0])
-		train_data['images'] = dataset.all_images[order[:train_size]]/256.0
-		train_data['labels'] = dataset.all_labels[order[:train_size]]
-		validation_data['images'] = dataset.all_images[order[train_size:]]/256.0
-		validation_data['labels'] = dataset.all_labels[order[train_size:]]
-	else:
-		train_data = {}
-		validation_data = {}
-		train_data['images'] = dataset.all_images/256.0
-		train_data['labels'] = dataset.all_labels
-	print "---->\n.....Done!"
-	return train_data, validation_data
+    """
+    load the mnist dataset from the skdata package.
+    """
+    print "---->\n.....Loading MNIST data from the skdata package"
+    dataset = view.OfficialImageClassification()
+    if train_size:
+        train_data = {}
+        validation_data = {}
+        order = permutation(dataset.all_images.shape[0])
+        train_data['images'] = dataset.all_images[order[:train_size]] / 256.
+        train_data['labels'] = dataset.all_labels[order[:train_size]]
+        validation_data['images'] = (dataset.all_images[order[train_size:]]
+                                     / 256.)
+        validation_data['labels'] = dataset.all_labels[order[train_size:]]
+    else:
+        train_data = {}
+        validation_data = {}
+        train_data['images'] = dataset.all_images / 256.
+        train_data['labels'] = dataset.all_labels
+    print "---->\n.....Done!"
+    return train_data, validation_data
 
 
-def make_vector_patches(data,nbatches,batch_size,field_size):
-	"""
-	Makes localized batches of patches of the MNIST data.
-	These can be randomly indexed to get a selection of 
-	"""
-	patches = []
-	for i in xrange(nbatches):
-		xs = np.zeros((batch_size,field_size**2))
-		for j in xrange(batch_size):
-			RAND_im = np.random.randint(data['images'].shape[0])
-			RAND_x = np.random.randint(data['images'].shape[1]-field_size)
-			RAND_y = np.random.randint(data['images'].shape[2]-field_size)
-			xs[j,:] = np.reshape(data['images'][RAND_im,RAND_x:RAND_x+field_size,RAND_y:RAND_y+field_size,:],(field_size**2,))
-		patches.append(xs)
-	return np.reshape(np.asarray(patches),(nbatches,batch_size,-1))
+def make_vector_patches(data, nbatches, batch_size, field_size):
+    """
+    Makes localized batches of patches of the MNIST data.
+    These can be randomly indexed to get a selection of
+    """
+    patches = np.zeros((nbatches, batch_size, field_size ** 2))
+    for i in xrange(nbatches):
+        xs = np.zeros((batch_size, field_size ** 2))
+        for j in xrange(batch_size):
+            RAND_im = np.random.randint(data['images'].shape[0])
+            RAND_x = np.random.randint(data['images'].shape[1] - field_size)
+            RAND_y = np.random.randint(data['images'].shape[2] - field_size)
+            xs[j, :] = np.reshape(data['images'][RAND_im, RAND_x:RAND_x
+                                + field_size, RAND_y:RAND_y + field_size, :],
+                                  (field_size ** 2,))
+        patches[i] = xs
+    return patches
 
-def make_localized_batches(data,nbatches,batch_size,field_size, stride):
-	"""
-	Makes localized batches of patches of the MNIST data.
-	These can be randomly indexed to get a selection of 
-	"""
-	batches = {}
-	for i in xrange(0,data['images'].shape[1]-field_size+1,stride):
-		for j in xrange(0,data['images'].shape[2]-field_size+1,stride):
-			batches[(i,j)] = make_unsupervised_batches(data['images'][:,i:i+field_size,j:j+field_size,:],nbatches,batch_size)
-	return batches
 
-def make_unsupervised_batches(data,nbatches,batch_size):
-	"""
-	Same as make_vector_batches, but doesn't include label data
-	"""
-	print '---->\n.....Putting data into vector-shaped batches'
-	assert nbatches*batch_size <= data.shape
-	permut = permutation(data.shape[0])
-	xdata = []
-	for i in xrange(nbatches):
-		xs = data[permut[i*batch_size:(i+1)*batch_size],:,:,:]
-		xdata.append(reshape(xs,(batch_size,prod(xs.shape)/batch_size)))
-	return np.reshape(np.asarray(xdata),(nbatches,batch_size,-1))
+def make_localized_batches(data, nbatches, batch_size, field_size, stride):
+    """
+    Makes localized batches of patches of the MNIST data.
+    These can be randomly indexed to get a selection of 
+    """
+    batches = {}
+    for i in xrange(0, data['images'].shape[1] - field_size + 1, stride):
+        for j in xrange(0, data['images'].shape[2] - field_size + 1, stride):
+            batches[(i, j)] = make_unsupervised_batches(data['images']
+                                [:, i:i + field_size, j:j + field_size, :],
+                                nbatches, batch_size)
+    return batches
 
-def make_vector_batches(data , nbatches, batch_size):
-	"""
-	Loads data in data into a list of numpy arrays with individual batches.
-	batches are in batch_size x data_dim format 
-	"""
-	print "---->\n.....Putting into vector-shaped batches"
-	assert nbatches*batch_size <= data['images'].shape[0]
-	batches = np.asarray([])
-	permut = permutation(data['images'].shape[0])
-	xdata = []
-	ydata = []
-	for i in range(nbatches):
-		xs = data['images'][permut[i*batch_size:(i+1)*batch_size],:,:,:]
-		xdata.append(reshape(xs,(batch_size,prod(xs.shape)/batch_size)))
-		ydata.append(data['labels'][permut[i*batch_size:(i+1)*batch_size]])
-	print "---->\n.....Done!"
-	return [np.reshape(np.asarray(xdata),(nbatches,batch_size,-1)),np.asarray(ydata)]
 
-def make_batches(data , nbatches, batch_size):
-	assert nbatches*batch_size <= data['images'].shape[0]
-	batches = []
-	permut = permutation(data['images'].shape[0])
-	for i in range(nbatches):
-		batch = {}
-		batch['x'] = data['images'][permut[i*batch_size:(i+1)*batch_size],:,:,:]
-		batch['y'] = data['labels'][permut[i*batch_size:(i+1)*batch_size]]
-		batches = batches+[batch]
-	return batches
+def make_unsupervised_batches(data, nbatches, batch_size):
+    """
+    Same as make_vector_batches, but doesn't include label data
+    """
+    print '---->\n.....Putting data into vector-shaped batches'
+    assert nbatches * batch_size <= data.shape
+    permut = permutation(data.shape[0])
+    xdata = []
+    for i in xrange(nbatches):
+        xs = data[permut[i * batch_size:(i + 1) * batch_size], :, :, :]
+        xdata.append(reshape(xs, (batch_size, prod(xs.shape) / batch_size)))
+    return np.reshape(np.asarray(xdata), (nbatches, batch_size, -1))
 
-if __name__=='__main__':
-	data,_ = load_data_mnist(train_size = 50000)
-	local_batches = make_vector_patches(data,10,10,14)
-	print local_batches.shape
+
+def make_vector_batches(data, nbatches, batch_size):
+    """
+    Loads data in data into a list of numpy arrays with individual batches.
+    batches are in batch_size x data_dim format 
+    """
+    print "---->\n.....Putting into vector-shaped batches"
+    assert nbatches * batch_size <= data['images'].shape[0]
+    permut = permutation(data['images'].shape[0])
+    xdata = []
+    ydata = []
+    for i in range(nbatches):
+        xs = data['images'][permut[i * batch_size:(i + 1) * batch_size],
+                                                                    :, :, :]
+        xdata.append(reshape(xs, (batch_size, prod(xs.shape) / batch_size)))
+        ydata.append(data['labels'][permut[i * batch_size:(i + 1)
+                                           * batch_size]])
+    print "---->\n.....Done!"
+    return [np.reshape(np.asarray(xdata), (nbatches, batch_size, -1)),
+            np.asarray(ydata)]
+
+
+def make_batches(data, nbatches, batch_size):
+    assert nbatches * batch_size <= data['images'].shape[0]
+    batches = []
+    permut = permutation(data['images'].shape[0])
+    for i in range(nbatches):
+        batch = {}
+        batch['x'] = data['images'][permut[i * batch_size:(i + 1)
+                                           * batch_size], :, :, :]
+        batch['y'] = data['labels'][permut[i * batch_size:(i + 1)
+                                           * batch_size]]
+        batches = batches + [batch]
+    return batches
+
+if __name__ == '__main__':
+    data, _ = load_data_mnist(train_size=50000)
+    local_batches = make_vector_patches(data, 10, 10, 14)
+    print local_batches.shape
